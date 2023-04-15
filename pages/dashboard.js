d57b64dfebc82fce21b7a9b57d5aa06514090c87
catthu <catthunh@gmail.com>
2023-04-15 13:40:26 +0200

    fix tailwind config

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,8 @@
 export const Dashboard = () => {
     return (
-        <div>Dashboard
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
+        <div className="bg-[#1C2928] text-white p-8 w-full">Dashboard
+        <h1 className="text-3xl font-bold" >
+            Zuzalu Value Dashboard
         </h1>
   </div>
     )
