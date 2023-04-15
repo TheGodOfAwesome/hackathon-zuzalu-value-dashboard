fc15cd849276bb1839cba8aa7cf5441f6d6ed9f0
catthu <catthunh@gmail.com>
2023-04-15 13:18:39 +0200

    tailwind css

---
+++ pages/dashboard.js
@@ -0,0 +1,9 @@
+export const Dashboard = () => {
+    return (
+        <div>Dashboard
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+  </div>
+    )
+}
